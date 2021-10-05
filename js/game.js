9522ca292d27af77e72546a56eef57baedcf6d3a
Helena216 <91907434+Helena216@users.noreply.github.com>
2021-10-05 20:06:05 +0300

    Added files

--- js/game.js
+++ js/game.js
@@ -1,14 +1,19 @@
 'use strict'
 
-// const MINE = '💣';
-// const FLAG = '🚩'
-
 const LAND = 'LAND';
-// var MINE = 'https://emojipedia.org/emoji/%F0%9F%92%A3/';
-// var FLAG = 'https://emojipedia.org/emoji/%F0%9F%9A%A9/';
 var MINE = 'MINE'
 var FLAG = 'FLAG'
 var gBoard;
+var gLevel = {
+	SIZE: 4,
+	MINES: 2
+}
+var gGame = {
+	isON: false,
+	shownCount: 0,
+	markedCount: 0,
+	secsPassed: 0
+}
 
 var MINE_IMG = '<img src="img2/mine.png" />';
 var FLAG_IMG = '<img src="img2/flag.png" />';
@@ -21,21 +26,55 @@ function initGame() {
 function buildBoard() {
 	// Create the Matrix
 	var board = createMat(4, 4)
-
 	// Put FLOOR everywhere and WALL at edges
+	var gNextId = 101
 	for (var i = 0; i < board.length; i++) {
 		for (var j = 0; j < board[0].length; j++) {
-			var cell = { type: LAND, gameElement: null, isShown : true };
+			var cell = {
+				id: (i + '-' + j),
+				type: LAND,
+				gameElement: null,
+				minesAroundCount: 4,
+				isShown: true,
+				isMine: false,
+				isMarked: true
+			};
 			board[i][j] = cell;
 		}
-    }
-        // Place the Balls (currently randomly chosen positions)
-        board[3][2].gameElement = MINE;
-        board[0][1].gameElement = MINE;
-    
-    console.log(board);
-    console.table(board);
+	}
+	board[3][2].gameElement = MINE;
+	board[0][1].gameElement = MINE;
+
+	console.log(board);
+	console.table(board);
 	return board;
+}
+
+function findMines(board, MINE) {
+	var res = [];
+	for (var i = 0; i < board.length; i++) {
+		if (i < 0 || i > board.length - 1) continue;
+		for (var j = 0; j < board[0].length; j++) {
+			if (j < 0 || j > board[0].length - 1) continue;
+			if (board[i][j] === MINE) {
+				board[i][j] = []
+				res.push(board[i][j])
+			}
+		}
+	}
+	return res;
+}
+
+function createMat(ROWS, COLS) {
+	var mat = []
+	for (var i = 0; i < ROWS; i++) {
+		var row = []
+		for (var j = 0; j < COLS; j++) {
+			row.push('')
+		}
+		mat.push(row)
+	}
+	return mat
 }
 
 function renderBoard(board) {
@@ -48,17 +87,16 @@ function renderBoard(board) {
 			var cellClass = getClassName({ i: i, j: j })
 
 			if (currCell.type === LAND) cellClass += ' land';
-//			else if (currCell.type === WALL) cellClass += ' wall';
 
 			strHTML += '\t<td class="cell ' + cellClass +
-				'"  onclick="moveTo(' + i + ',' + j + ')" >\n';
+				'"  onclick="cellClicked(' + i + ',' + j + ')" >\n';
 
 			if (currCell.gameElement === MINE) {
 				strHTML += MINE_IMG;
-                //strHTML += '@'
+				//strHTML += '@'
 			} else if (currCell.gameElement === FLAG) {
-                //strHTML += '*'
-				 strHTML += FLAG_IMG;
+				//strHTML += '*'
+				strHTML += FLAG_IMG;
 			}
 
 			strHTML += '\t</td>\n';
@@ -66,13 +104,27 @@ function renderBoard(board) {
 		strHTML += '</tr>\n';
 	}
 
-	console.log('strHTML is:');
-	console.log(strHTML);
-	var elBoard = document.querySelector('.board');
-	elBoard.innerHTML = strHTML;
+	function cellClicked(elCell, i, j) {
+		var cell = gBoard[i][j]
+		console.log('Cell clicked: ', i, j, cell);
+		if (cell = MINE) endsGame()
+		if (cell = isMarked) unmarkCell()
+		if (cell = !isShown) showCell();
+	}
 }
+
+console.log('strHTML is:');
+console.log(strHTML);
+var elBoard = document.querySelector('.board');
+elBoard.innerHTML = strHTML;
+
+
+
 
 function getClassName(location) {
 	var cellClass = 'cell-' + location.i + '-' + location.j;
 	return cellClass;
 }
+
+
+
